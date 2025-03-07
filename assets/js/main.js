4c46ea08ada5c4fa872af31aa5c1f05d04a20253
SandyPears <adam.cordelia1@gmail.com>
2025-03-07 14:41:11 +1100

    Fade in test

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,6 +115,22 @@
 
 		}
 
+	$(document).on("scroll", function () {
+		var pageTop = $(document).scrollTop();
+		var pageBottom = pageTop + $(window).height();
+		var tags = $(".tag");
+
+		for (var i = 0; i < tags.length; i++) {
+			var tag = tags[i];
+
+			if ($(tag).position().top < pageBottom) {
+				$(tag).addClass("visible");
+			} else {
+				$(tag).removeClass("visible");
+			}
+		}
+	});
+
 	// Scrolly.
 		$('.scrolly').scrolly({
 			speed: 1000
