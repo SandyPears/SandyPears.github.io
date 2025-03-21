d39e76571ae3c355fc868a75a5e55df815ef7205
SandyPears <adam.cordelia1@gmail.com>
2025-03-21 14:22:07 +1100

    Slideshow

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -1,14 +1,26 @@
 let slideIndex = 0;
-showSlides();
 
 function showSlides() {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let slideWidth = slides[0].clientWidth; // Get the width of a slide
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(-${slideIndex * slideWidth}px)`; // Scroll effect
   }
   slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1; }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 3 seconds
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
+// Manual navigation functions
+function plusSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let slideWidth = slides[0].clientWidth; // Get the width of a slide
+  slideIndex += n;
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  if (slideIndex < 0) { slideIndex = slides.length - 1; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(-${slideIndex * slideWidth}px)`; // Scroll effect
+  }
+}
+
+window.onload = showSlides;
